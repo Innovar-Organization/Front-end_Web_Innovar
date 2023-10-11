85ffe12a21ac65aab10a6ee23c3cec3da671def1
Wedleysilva <Wedleysilva2105@gmail.com>
2023-10-11 15:05:34 -0300

    Components inseridos na App

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+    <div class="header">
+      <h2 class="header-text">Este é o Header</h2>
+    </div>
+  </template>
+  
+  <style scoped>
+  .header {
+    background-color: cyan;
+    height: 200px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+  
+  .header-text {
+    color: white;
+  }
+  </style>
+  
---
+++ src/screens/LoginScreen.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+    <div>ola mundo</div>
+</template> 
+<style>
+</style>
