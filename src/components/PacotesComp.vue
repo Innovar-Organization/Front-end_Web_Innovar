e43feef5c7f241f7aa460e5e30f117014b2df53f
GuilhermeHalter <guisam.halter@gmail.com>
2023-12-04 09:18:17 -0300

    dando erro em post de imagem

--- src/components/PacotesComp.vue
+++ src/components/PacotesComp.vue
@@ -1,21 +1,42 @@
 <script setup>
 import { reactive, ref, onMounted } from 'vue';
 import api from "@/plugins/axios";
+import pacoteService from '@/services/pacote.js'
+import imageService from '@/services/images.js'
 
+
+const coverUrl = ref('')
+const file = ref(null)
 const Pacotes = reactive({
   nome: '',
   descricao: '',
   imagem: ''
 });
 
+
 const adicionarPacotes = async () => {
   try {
     const response = await api.post('/pacotes/', Pacotes);
+    const image = await imageService.uploadImage(file.value)
+    Pacotes.cover_attachment_key = image.attachment_key
+    await pacoteService.savePacote(Pacotes)
+    Object.assign(Pacotes, {
+      id: '',
+      nome: '',
+      descricao: '',
+      imagem: ''
+    })
+  showForm.value = false
     console.log('Resposta do servidor:', response.data);
   }catch(error) {
-    console.error('Erro ao adicionar horario:', error);
+    console.error('Erro ao adicionar pacote:', error);
   }
 };
+
+function onFileChange(e) {
+  file.value = e.target.files[0]
+  coverUrl.value = URL.createObjectURL(file.value)
+}
 
 const pacote = ref([]);
 
@@ -23,6 +44,8 @@ onMounted(async () =>{
   const response = await api.get('/pacotes/');
   pacote.value = response.data.results;
 })
+
+const showForm = ref(false)
 </script>
 
 <template>
@@ -33,8 +56,16 @@ onMounted(async () =>{
     <label for="procedimentoNome">Descrição do Pacote:</label>
     <input v-model="Pacotes.descricao" class="nome-horaInicio"  >
 
-    <label for="procedimentoNome">Imagem:</label>
-    <input v-model="Pacotes.imagem" class="nome-horaFim">
+    <form class="form">
+        <div class="row">
+          <div id="preview">
+            <input type="file" @change="onFileChange" />
+            <div class="cover">
+              <img v-if="coverUrl" :src="coverUrl" />
+            </div>
+          </div>
+        </div>
+      </form>
   
   <div class="button-container">
   <div class="botão">
@@ -43,8 +74,6 @@ onMounted(async () =>{
   </div>
   </div>
 </template>
-
-
 <style scoped>
 
 .coluna-cotainer {
